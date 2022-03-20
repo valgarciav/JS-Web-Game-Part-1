eabdb16c3bad24f5f1fee4b346bd369445326214
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-03-20 14:27:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-let greenCharacter = document.createElement("img");
-greenCharacter.src = "assets/green-character.gif";
-greenCharacter.style.position = "fixed";
-greenCharacter.style.left = "100px";
-greenCharacter.style.bottom = "100px";
-document.body.append(greenCharacter);
+function newImage(url, left, bottom) {
+    let item = document.createElement('img')
+    item.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(image);
+    return image;
+}
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-let;
+function newItem(url, left, bottom){
+        let object = newImage(url, left, bottom)
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+sword.addEventListener('click', function() {sword.remove()
+})
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
