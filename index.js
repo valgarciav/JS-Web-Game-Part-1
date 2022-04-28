89a49a7e01007e6e6692895a4172a1b91bdd24ef
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-04-27 20:20:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 function newImage(url, left, bottom) {
-    let item = document.createElement('img')
-    item.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(image);
-    return image;
+  let item = document.createElement("img");
+  item.src = url;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(image);
+  return image;
 }
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+function newItem(url, left, bottom) {
+  let object = newImage(url, left, bottom);
 
-function newItem(url, left, bottom){
-        let object = newImage(url, left, bottom)
+  object.addEventListener("dblclick", () => {
+    object.remove();
+  });
+}
 
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
-
-sword.addEventListener('click', function() {sword.remove()
-})
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+sword.addEventListener("click", function () {
+  sword.remove();
+});
+newItem("assets/sword.png", 500, 405);
+newItem("assets/shield.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
